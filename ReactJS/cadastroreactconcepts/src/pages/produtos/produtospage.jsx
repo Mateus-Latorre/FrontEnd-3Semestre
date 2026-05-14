c61b3673bc540284e2863693f55377c3b8644139
MATEUS LATORRE <50055487858@info.local>
2026-05-14 13:07:52 -0300

    Continuada as atividades de Produto

--- ReactJS/cadastroreactconcepts/src/pages/produtos/produtospage.jsx
+++ ReactJS/cadastroreactconcepts/src/pages/produtos/produtospage.jsx
@@ -1,15 +1,37 @@
 import "./produtospage.css";
+import imagem from "../../assets/hero.png";
+import { useState } from "react";
 const Produtos = () => {
+    const [listaProdutos, setListaProdutos] = useState([
+        {id: 1, nome: 'Bomba', descricao: 'Bomba legal', preco: 67.67, imagem: imagem},
+        {id: 1, nome: 'Bomba', descricao: 'Bomba legal', preco: 67.67, imagem: imagem},
+        {id: 1, nome: 'Bomba', descricao: 'Bomba legal', preco: 67.67, imagem: imagem},
+    ]);
+
     return (
-        <body className="produtos">
-        <h1 className="texto-produto">Página de Produtos</h1>
-        <form action="">
-            <h1>Cadastro de Produtos</h1>
-            <input type="text" placeholder="Nome do Produto" />
-            <input type="text" placeholder="Descrição" />
-            <input type="file" placeholder="Imagem"/>
-        </form>
-        </body>
+        <main className="produtos">
+            <h1 className="texto-produto">Cadastro de Produtos</h1>
+            <div className="div-cadastro">
+                <form action="" className="cadastro-formulario">
+                    <input className="input-formulario" type="text" placeholder="Nome do Produto" />
+                    <input className="input-formulario input-descricao" type="text" placeholder="Descrição" />
+                    <input className="input-formulario" type="number" placeholder="Preço" />
+                    <label className="label_upload" htmlFor="arquivo">Imagem do Produto</label>
+                    <input className="input-formulario btn_upload" id="arquivo" type="file" placeholder="Imagem do Produto" />
+                    <button className="btn_cadastrar">Cadastrar</button>
+                </form>
+                <div className="div-produto">
+                <section className="secao-produto">
+                    <article className="card__produto">
+                        <img src={imagem} className="img_produto"/>
+                        <span><strong>Nome:</strong>Bomba</span>
+                        <span><strong>Descrição:</strong>Bomba legal</span>
+                        <span><strong>Preço:</strong>R$67,67</span>
+                    </article>
+                </section>
+                </div>
+            </div>
+        </main>
     );
 };
 
