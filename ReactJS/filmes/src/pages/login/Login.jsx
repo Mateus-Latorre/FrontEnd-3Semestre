3fe40ff65e320916a476f14b21f6e13885a680a3
MATEUS LATORRE <50055487858@info.local>
2026-06-02 16:15:29 -0300

    Criada a tela login

--- ReactJS/filmes/src/pages/login/Login.jsx
+++ ReactJS/filmes/src/pages/login/Login.jsx
@@ -1,12 +1,31 @@
 import "./Login.css";
+import Logo from "../../assets/img/logo.svg";
+import Botao from "../../components/botao/Botao";
 import Header from "../../components/header/Header";
 import Fotter from "../../components/footer/Footer";
 const Login = () => {
     return (
         <div className="container">
-            <Header />
-            <h1>Login</h1>
-            <Fotter />
+            <main className= "main_login">
+          <div className="banner"></div>
+          <section className="section_login">
+            <img src={Logo} alt="Logo do Filmoteca"/>
+            <form action="" className="form_login">
+                <h1>Login</h1>
+                <div className="campos_login">
+                    <div className="campo_input">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name="email" placeholder="Digite seu e-mail"/>
+                    </div>
+                    <div className="campo_input">
+                        <label htmlFor="senha">Senha:</label>
+                        <input type="password" name="senha" placeholder="Digite sua senha"/>
+                    </div>
+                </div>
+                <Botao nomeDoBotao="Entrar"/>
+            </form>
+          </section>
+        </main>
         </div>
     );
 }
